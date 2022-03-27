11d5d0f010bf586e2f3c39891c3414a759e83f2e
rtambun <richson.tambun@gmail.com>
2022-03-27 09:53:21 +0800

    Rename UrlShortenerResponse to UrlResponse to make it generic.

--- url-shortener-service/src/test/java/com/rtambun/urlshorterner/urlshortenerservice/dto/UrlShortenerResponseTest.java
+++ url-shortener-service/src/test/java/com/rtambun/urlshorterner/urlshortenerservice/dto/UrlShortenerResponseTest.java
@@ -3,20 +3,19 @@ package com.rtambun.urlshorterner.urlshortenerservice.dto;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class UrlShortenerResponseTest {
 
     @Test
     void urlShortenerResponse_AllArgsConstructor() {
-        UrlShortenerResponse actual = new UrlShortenerResponse("shortUrl", "anyUrl");
+        UrlResponse actual = new UrlResponse("shortUrl", "anyUrl");
         assertThat(actual.getShortUrl()).isEqualTo("shortUrl");
         assertThat(actual.getLongUrl()).isEqualTo("anyUrl");
     }
 
     @Test
     void urlShortenerResponse_NoArgsConstructor() {
-        UrlShortenerResponse actual = new UrlShortenerResponse();
+        UrlResponse actual = new UrlResponse();
         actual.setShortUrl("shortUrl");
         actual.setLongUrl("anyUrl");
         assertThat(actual.getShortUrl()).isEqualTo("shortUrl");
